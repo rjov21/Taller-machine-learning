5401d95932c3ef06ba47baca4752fffa199b30e7
rjo21 <robinsonor4@gmail.com>
2022-04-29 12:34:52 -0500

    sexto commit

--- weatherAUS.py
+++ weatherAUS.py
@@ -21,35 +21,53 @@ data = pd.read_csv(url)
 data.drop(['Date', 'MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine', 
            'WindGustSpeed', 'Pressure9am', 'Humidity9am', 'Humidity3pm', 
            'Pressure3pm', 'Temp9am', 'Temp3pm'], axis= 1, inplace = True)
-data.drop(['Date'], axis=1, inplace=True)
 data['Location'].replace(['Canberra', 'Sydney', 'Perth', 'Darwin', 'Hobart',
                           'Brisbane', 'Adelaide', 'Bendigo', 'Townsville', 'AliceSprings', 
                           'MountGambier', 'Ballarat', 'Launceston', 'Albany', 'Albury',
                           'MelbourneAirport', 'PerthAirport', 'Mildura', 'SydneyAirport', 
                           'Nuriootpa', 'Sale', 'Watsonia', 'Tuggeranong', 'Portland',
                           'Woomera', 'Cobar', 'Cairns', 'Wollongong', 'GoldCoast',
-                          'WaggaWagga', 'NorfolkIsland', 'Penrith', 'SalmonGums', 'Newcastle',
+                          'WaggaWagga', 'NorfolkIsland', 'Penrith', 'SalmonGums',
+                          'Newcastle',
                           'CoffsHarbour', 'Witchcliffe', 'Richmond', 'Dartmoor', 
-                          'NorahHead', 'BadgerysCreek', 'MountGinini', 'Moree', 'Walpole',
-                          'PearceRAAF', 'Williamtown', 'Melbourne', 'Nhil', 'Katherine',
-                          'Uluru'],
+                          'NorahHead', 'BadgerysCreek', 'MountGinini', 'Moree',
+                          'Walpole','PearceRAAF', 'Williamtown', 'Melbourne', 'Nhil',
+                          'Katherine','Uluru'],
                          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
                           19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
-                          35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49], inplace=True)
-
+                          35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
+                         inplace=True)
+data['WindGustDir'].replace(['W', 'SE', 'E', 'N', 'SSE', 'S', 'WSW', 'SW', 'SSW', 'WNW',
+                             'NW', 'ENE', 'ESE', 'NE', 'NNW', 'NNE'], 
+                            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+                            inplace=True)
+data['WindDir9am'].replace(['W', 'SE', 'E', 'N', 'SSE', 'S', 'WSW', 'SW', 'SSW', 'WNW',
+                             'NW', 'ENE', 'ESE', 'NE', 'NNW', 'NNE'], 
+                            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+                            inplace=True)
+data['WindDir3pm'].replace(['W', 'SE', 'E', 'N', 'SSE', 'S', 'WSW', 'SW', 'SSW', 'WNW',
+                             'NW', 'ENE', 'ESE', 'NE', 'NNW', 'NNE'], 
+                            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+                            inplace=True)
+data['RainToday'].replace(['No', 'Yes'], [0, 1], inplace=True)
+risk_rangos = [0, 50, 100, 150, 200, 250, 300, 350, 400]
+risk_nombres = ['1', '2', '3', '4', '5', '6', '7', '8']
+data.RISK_MM = pd.cut(data.RISK_MM, risk_rangos, labels=risk_nombres)
+data['RainTomorrow'].replace(['No', 'Yes'], [0, 1], inplace=True)
+data.dropna(axis=0,how='any', inplace=True)
 
 #partir la data en dos
 
-data_train = data[:206]
-data_test = data[206:]
+data_train = data[:17296]
+data_test = data[17296:]
 
-x = np.array(data_train.drop(['Outcome'], 1))
-y = np.array(data_train.Outcome) 
+x = np.array(data_train.drop(['RainTomorrow'], 1))
+y = np.array(data_train.RainTomorrow) 
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-x_test_out = np.array(data_test.drop(['Outcome'], 1))
-y_test_out = np.array(data_test.Outcome)
+x_test_out = np.array(data_test.drop(['RainTomorrow'], 1))
+y_test_out = np.array(data_test.RainTomorrow)
 
 
 
